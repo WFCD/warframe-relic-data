18370bc3f782c99d6eaf0c76c65d82768596c2b2
Soundofdarkness <imsoundoflight@gmail.com>
2019-09-10 21:07:10 +0200

    Update Script and Hope

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+if [ -z "$(git status --porcelain)" ];then
+  echo "Changes found => pushing"
+  WARFRAME_BUILD="$(cat ./data/version)"
+  git config user.name "Titania CI"
+  git config user.email "titaniaci@sleepylux.xyz"
+  git commit -m "📝 Update(items) for Warframe Version $WARFRAME_BUILD"
+  git remote rm origin
+  git remote add origin https://"$ACCESS_TOKEN":x-oauth-basic@github.com/TitaniaProject/warframe-relic-data
+  git push -u origin master
+  echo "Sucessfully updated Items to Warframe version $WARFRAME_BUILD"
+else
+  echo "No changes found => Finished."
+fi
