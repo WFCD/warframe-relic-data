ce340c58cf9d29a02549dd97c3f372610da61653
Soundofdarkness <imsoundoflight@gmail.com>
2019-09-10 21:40:22 +0200

    Next try to fix

--- update.sh
+++ update.sh
@@ -7,9 +7,8 @@ if [ -n "$(git status --porcelain)" ];then
   git config user.email "titaniaci@sleepylux.xyz"
   git add .
   git commit -m "📝 Update(items) for Warframe Version $WARFRAME_BUILD"
-  git remote rm origin
-  git remote add origin "https://Soundofdarkness:$ACCESS_TOKEN@github.com/TitaniaProject/warframe-relic-data"
-  git push -u origin master
+  git remote set-url origin "https://$ACCESS_TOKEN@github.com/TitaniaProject/warframe-relic-data"
+  git push push
   echo "Sucessfully updated Items to Warframe version $WARFRAME_BUILD"
 else
   echo "No changes found => Finished."
