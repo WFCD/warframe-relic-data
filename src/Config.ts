12e9f57e0d31b300833ea0469faec85cdbb28eb7
Soundofdarkness <imsoundoflight@gmail.com>
2022-08-12 18:39:41 +0200

    Changed Drop Data urls to drops.warframestat.us

--- src/Config.ts
+++ src/Config.ts
@@ -1,9 +1,9 @@
 
 const config = {
-    warframeRelicDropUrl: "https://raw.githubusercontent.com/WFCD/warframe-drop-data/main/data/relics.json",
+    warframeRelicDropUrl: "http://drops.warframestat.us/data/relics.json",
     warframeMarketItemUrl: "https://api.warframe.market/v1/items",
     warframeItemsUrl: "https://raw.githubusercontent.com/WFCD/warframe-items/master/data/json/Relics.json",
-    warframeRelicDropInfoUrl: "https://raw.githubusercontent.com/WFCD/warframe-drop-data/main/data/info.json",
+    warframeRelicDropInfoUrl: "http://drops.warframestat.us/data/info.json",
     warframePatchlogsUrl: "https://raw.githubusercontent.com/WFCD/warframe-patchlogs/master/data/patchlogs.json"
 };
 
