da6800d8f0966bd127ab0654a4f98247f27bc7b0
Tobiah <tobiah@pm.me>
2022-09-25 22:21:12 -0500

    re-add exports for types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
 import Generator from './Generator';
 import VersionManager from './VersionManager';
+import {
+  TitaniaRelic,
+  TitaniaRelicLocation,
+  TitaniaRelicReward,
+  TitaniaRelicRewardItem,
+  TitaniaRelicVaultedInfo,
+  TitaniaWFMInfo,
+} from './Types';
 
-export { Generator, VersionManager };
+export {
+  Generator,
+  VersionManager,
+  TitaniaRelic,
+  TitaniaRelicLocation,
+  TitaniaRelicReward,
+  TitaniaRelicRewardItem,
+  TitaniaRelicVaultedInfo,
+  TitaniaWFMInfo,
+};
